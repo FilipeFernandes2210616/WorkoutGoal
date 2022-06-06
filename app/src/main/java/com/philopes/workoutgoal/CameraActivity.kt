fb77cabcb121846f64c5068cbbb390f6471ef39a
jmachado100 <jose-machado96@hotmail.com>
2022-06-06 21:05:30 +0100

    Pose Detection Base

---
+++ app/src/main/java/com/philopes/workoutgoal/CameraActivity.kt
@@ -0,0 +1,231 @@
+package com.philopes.workoutgoal
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.demo.kotlin.posedetector.PoseDetectorProcessor
+import com.philopes.workoutgoal.Utils.CameraSource
+import com.philopes.workoutgoal.Utils.CameraSourcePreview
+import com.philopes.workoutgoal.Utils.GraphicOverlay
+import com.philopes.workoutgoal.Utils.PreferenceUtils
+import java.io.IOException
+import java.util.ArrayList
+
+class CameraActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener  {
+
+    private var cameraSource: CameraSource? = null
+    private var preview: CameraSourcePreview? = null
+    private var graphicOverlay: GraphicOverlay? = null
+    private var selectedModel = POSE_DETECTION
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        preview = findViewById(R.id.preview_view)
+        if (preview == null) {
+            Log.d(TAG, "Preview is null")
+        }
+
+        graphicOverlay = findViewById(R.id.graphic_overlay)
+        if (graphicOverlay == null) {
+            Log.d(TAG, "graphicOverlay is null")
+        }
+
+        if (!allRuntimePermissionsGranted()) {
+            getRuntimePermissions()
+        }
+
+
+
+        val facingSwitch = findViewById<ToggleButton>(R.id.facing_switch)
+        facingSwitch.setOnCheckedChangeListener(this)
+
+
+        /*
+        val settingsButton = findViewById<ImageView>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            val intent = Intent(applicationContext, SettingsActivity::class.java)
+            intent.putExtra(SettingsActivity.EXTRA_LAUNCH_SOURCE, LaunchSource.LIVE_PREVIEW)
+            startActivity(intent)
+        }
+
+         */
+
+        createCameraSource(selectedModel)
+    }
+
+    @Synchronized
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        selectedModel = parent?.getItemAtPosition(pos).toString()
+        Log.d(TAG, "Selected model: $selectedModel")
+        preview?.stop()
+        createCameraSource(selectedModel)
+        startCameraSource()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // Do nothing.
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+        Log.d(TAG, "Set facing")
+        if (cameraSource != null) {
+            if (isChecked) {
+                cameraSource?.setFacing(CameraSource.CAMERA_FACING_FRONT)
+            } else {
+                cameraSource?.setFacing(CameraSource.CAMERA_FACING_BACK)
+            }
+        }
+        preview?.stop()
+        startCameraSource()
+    }
+
+    private fun createCameraSource(model: String) {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = CameraSource(this, graphicOverlay)
+        }
+        try {
+            when (model) {
+                POSE_DETECTION -> {
+                    val poseDetectorOptions = PreferenceUtils.getPoseDetectorOptionsForLivePreview(this)
+                    Log.i(TAG, "Using Pose Detector with options $poseDetectorOptions")
+                    val shouldShowInFrameLikelihood =
+                        PreferenceUtils.shouldShowPoseDetectionInFrameLikelihoodLivePreview(this)
+                    val visualizeZ = PreferenceUtils.shouldPoseDetectionVisualizeZ(this)
+                    val rescaleZ = PreferenceUtils.shouldPoseDetectionRescaleZForVisualization(this)
+                    val runClassification = PreferenceUtils.shouldPoseDetectionRunClassification(this)
+                    cameraSource!!.setMachineLearningFrameProcessor(
+                        PoseDetectorProcessor(
+                            this,
+                            poseDetectorOptions,
+                            shouldShowInFrameLikelihood,
+                            visualizeZ,
+                            rescaleZ,
+                            runClassification,
+                            /* isStreamMode = */ true
+                        )
+                    )
+                }
+                else -> Log.e(TAG, "Unknown model: $model")
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "Can not create image processor: $model", e)
+            Toast.makeText(
+                applicationContext,
+                "Can not create image processor: " + e.message,
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private fun startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (preview == null) {
+                    Log.d(TAG, "resume: Preview is null")
+                }
+                if (graphicOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null")
+                }
+                preview!!.start(cameraSource, graphicOverlay)
+            } catch (e: IOException) {
+                Log.e(TAG, "Unable to start camera source.", e)
+                cameraSource!!.release()
+                cameraSource = null
+            }
+        }
+    }
+
+    private fun allRuntimePermissionsGranted(): Boolean {
+        for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun getRuntimePermissions() {
+        val permissionsToRequest = ArrayList<String>()
+        for (permission in REQUIRED_RUNTIME_PERMISSIONS) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    permissionsToRequest.add(permission)
+                }
+            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i(TAG, "Permission granted: $permission")
+            return true
+        }
+        Log.i(TAG, "Permission NOT granted: $permission")
+        return false
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+        createCameraSource(selectedModel)
+        startCameraSource()
+    }
+
+    /** Stops the camera. */
+    override fun onPause() {
+        super.onPause()
+        preview?.stop()
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        if (cameraSource != null) {
+            cameraSource?.release()
+        }
+    }
+
+    companion object {
+        private const val POSE_DETECTION = "Pose Detection"
+
+        private const val TAG = "LivePreviewActivity"
+
+        private const val PERMISSION_REQUESTS = 1
+
+        private val REQUIRED_RUNTIME_PERMISSIONS =
+            arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+    }
+}
